e0386d68b4c50be505aec4e9757e788ab3b65feb
unknown <hthrkuh8@gmail.com>
2021-06-22 20:05:53 +0300

    Updates

---
+++ precache-manifest.4a4ed1626db97c103c4c8a7228eec172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fdd3a4096f5cb7908cbde9d8cc88f3",
+    "url": "/Reactjs--Bitcoin/index.html"
+  },
+  {
+    "revision": "bd6bd8065942562548ea",
+    "url": "/Reactjs--Bitcoin/static/css/2.3a0c7bd3.chunk.css"
+  },
+  {
+    "revision": "bbf4e5913ed586a377bc",
+    "url": "/Reactjs--Bitcoin/static/css/main.b459c017.chunk.css"
+  },
+  {
+    "revision": "bd6bd8065942562548ea",
+    "url": "/Reactjs--Bitcoin/static/js/2.a0beab3b.chunk.js"
+  },
+  {
+    "revision": "bbf4e5913ed586a377bc",
+    "url": "/Reactjs--Bitcoin/static/js/main.055ddba1.chunk.js"
+  },
+  {
+    "revision": "0a7982a2ac449a72183f",
+    "url": "/Reactjs--Bitcoin/static/js/runtime~main.9b8a04e0.js"
+  }
+]);
