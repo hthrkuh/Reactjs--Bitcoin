f2a25e1baf1a47f685c9bbd6faf7003ca5407346
unknown <hthrkuh8@gmail.com>
2021-06-22 19:31:28 +0300

    Updates

---
+++ precache-manifest.d80b2340e4eeda4c9bc5291e970cb9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe831b588ca51fc0c535a3252bbbcc5",
+    "url": "/Reactjs--Bitcoin/index.html"
+  },
+  {
+    "revision": "45edf11909399aec366a",
+    "url": "/Reactjs--Bitcoin/static/css/2.2cc849e2.chunk.css"
+  },
+  {
+    "revision": "f0addb99b0b38fd6c602",
+    "url": "/Reactjs--Bitcoin/static/css/main.b459c017.chunk.css"
+  },
+  {
+    "revision": "45edf11909399aec366a",
+    "url": "/Reactjs--Bitcoin/static/js/2.cba5c7c4.chunk.js"
+  },
+  {
+    "revision": "f0addb99b0b38fd6c602",
+    "url": "/Reactjs--Bitcoin/static/js/main.9826465b.chunk.js"
+  },
+  {
+    "revision": "0a7982a2ac449a72183f",
+    "url": "/Reactjs--Bitcoin/static/js/runtime~main.9b8a04e0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Reactjs--Bitcoin/precache-manifest.18f5a7c68ab5a3ca2da2e00f13784d46.js"
+  "/Reactjs--Bitcoin/precache-manifest.d80b2340e4eeda4c9bc5291e970cb9c4.js"
 );
 
 self.addEventListener('message', (event) => {
